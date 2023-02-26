89570f6ad4e4b174a22a37f77b24fe5235c6a1fe
Mhummad Mutahir <121317971+MRMutahir@users.noreply.github.com>
2023-02-27 04:28:20 +0500

    Add files via upload
    xyz

---
+++ array.js
@@ -0,0 +1,96 @@
+// let cars = ['toyota', 'Lambargini', 'Swift', 'KIA', 'Mheran']
+// console.log(cars)
+// console.log(cars.length)
+// console.log(cars.push('Oddi'))
+// console.log(cars)
+// let addLastValue = cars.push('Oddi')
+// console.log(addLastValue)
+// console.log(cars)
+// console.log(cars.pop())
+// let cityOfPakistan = ['Karachi', 'Multan', 'Lahore', ' PakPatan']
+// document.write(cityOfPakistan)
+// console.log(cityOfPakistan.unshift('Qutta'))
+
+// console.log(cityOfPakistan)
+// console.log(cityOfPakistan.shift())
+// console.log(cityOfPakistan.length)
+// console.log(cityOfPakistan.indexOf("Lahore"))
+// console.log(Array.isArray('Qutta'))
+// let num = 5;
+// num += ++num;
+// console.log(num)
+// let x = 9;
+// let y = '9';
+
+// console.log(x == y)
+// console.log(x === y)
+// let name = ['Mutahir'];
+// console.log(name.indexOf('Mutahir'))
+
+// let firstName = 'Mutahir';
+// let lastName = 'Kareem';
+// console.log(`My name is ${firstName}.
+// My Father  
+// name is ${lastName}.`)
+// function city_1(a) {
+//     return a+b
+    
+// }
+// city_1('KarachiKings','Shikarpor')
+// function hottel(a, b, c) {
+// return(hottel)
+
+// }
+
+// function school(a,b) {
+    
+//     return `aj  ${a}   ${b} aya hen `
+// }
+// console.log(  school('Mutahir','paresnt'))
+// console.log(school('taha','Pass'))
+//  let community =(x,y,z,u)=>{
+//     return ` ${x}  add ${y} and then divid by ${z} finally ${u}
+//     ites answer
+//     ${x+y/z%u} 
+//     `
+//  }
+//  console.log (community(50,50 ,5 ,2)) 
+//  const num_sec = (a,b,c ) =>{
+//     return a+b*c
+
+//  }
+//  console.log (num_sec(10,5,2))
+
+//  let subject_matric = ['Bio', 'Che', 'Math', 'Phy ', 'Urdu']
+// console.log(subject_matric)
+//  subject_matric [5]='Pst'
+
+//  let Medicine = ['SofVac', 'Panadol' ]
+
+// console.log (Medicine.push('relif'))
+// console.log(Medicine)
+// console.log(Medicine.pop())
+// console.log(Medicine)
+//  let Cake = ['Choclate', 'Stubarry','Vanela' ]
+//  console.log(Cake)
+//  console.log(Cake.unshift('Mutahir'))
+//  console.log(Cake)
+// console.log(Cake.shift())
+// console.log(Cake)
+ // var sub = (subject_matric.length-3)
+// console.log(sub)
+//  let x = 2;
+//  let y = 2;
+// x += ++x
+// console.log (x)
+ 
+//  let Friends = ['Raza', 'Abdul', 'Azmir', 'Mutahir']
+//  console.log(Friends)
+//  var Friends_alg = (Friends.slice(1,4))
+//  console.log(Friends_alg)
+//  let car = ['Civic', 'FourTunor','Carola','TKS','Metropolitan']
+//  car.splice(1,3,'Raza')
+//  console.log(car)
+//  let  shop = ['Milk','Oil','Meat','Potato']
+//  var shop_2 = shop.slice(1,3)
+//  console.log(shop_2)
